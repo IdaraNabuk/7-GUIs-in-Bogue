22cb490dbd9f0be90114ee834c33f2531bcc2bb5
Richie Nabuk <richienabuk@gmail.com>
2023-12-14 00:31:31 +0100

    Adds a counter

---
+++ bogue_counter/_build/default/bin/main.ml
@@ -0,0 +1,23 @@
+open Bogue
+module W = Widget
+
+let counter = ref 0
+
+let increment () =
+  counter := !counter + 1
+
+let decrement () =
+  counter := !counter - 1
+
+let update c n =
+    W.set_text c (string_of_int n)
+
+  let () =
+  let label = W.label "Cookies:" in
+  let count = W.label "0 " in
+  let inc_button = W.button ~action:(fun _ -> increment (); update count !counter) "Increment" in
+  let dec_button = W.button ~action:(fun _ -> decrement (); update count !counter) "Decrement" in
+  Layout.flat_of_w ~name:"Counter tutorial"
+    ~align:Draw.Center [label; count; inc_button; dec_button]
+  |> Bogue.of_layout
+  |> Bogue.run
