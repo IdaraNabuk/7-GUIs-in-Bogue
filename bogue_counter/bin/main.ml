033e70171bf1a7ff4229cb89cafb546dc1ae3f41
IdaraNabuk <idaranabuk@gmail.com>
2024-02-08 00:19:47 +0100

    Add gitignore file and update circle drawer

--- bogue_counter/bin/main.ml
+++ bogue_counter/bin/main.ml
@@ -3,10 +3,6 @@ module W = Widget
 
 let counter = ref 0
 
-let increment () = counter := !counter + 1
-
-let decrement () = counter := !counter - 1
-
 let update c n = W.set_text c (string_of_int n)
 
 let () =
@@ -14,12 +10,12 @@ let () =
   let count = W.label "0 " in
   let inc_button =
     W.button
-      ~action:(fun _ -> increment () ; update count !counter)
+      ~action:(fun _ -> incr counter ; update count !counter)
       "Increment"
   in
   let dec_button =
     W.button
-      ~action:(fun _ -> decrement () ; update count !counter)
+      ~action:(fun _ -> decr counter ; update count !counter)
       "Decrement"
   in
   Layout.flat_of_w ~name:"Counter tutorial" ~align:Draw.Center
