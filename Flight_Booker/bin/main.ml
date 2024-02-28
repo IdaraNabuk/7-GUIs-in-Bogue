05615d977306582d85150210b7014abba24b2146
IdaraNabuk <idaranabuk@gmail.com>
2024-02-28 17:05:26 +0100

    Create flight booker gui

--- Flight_Booker/bin/main.ml
+++ Flight_Booker/bin/main.ml
@@ -2,18 +2,25 @@ open Bogue
 module W = Widget
 module L = Layout
 
-let names = ref []
+(* let names = ref [] *)
 
 let () =
-  let input = W.text_input ~prompt:"Enter name" () in
-  let create_button = W.button "Create" in
-  let box = W.label ~size:15 "Names: " in
-  let _ = W.on_click ~click: (fun _ ->
-    let name = W.get_text input in
-    names := name :: !names;
-    W.set_text box (String.concat ", " !names);
-  ) create_button in
-  Layout.tower_of_w ~name:"CRUD" ~w:400 ~align:Draw.Center
-  [box; input; create_button;]
+let style = Style.(of_bg (color_bg Draw.(opaque (find_color "white")))
+|> with_border
+  (mk_border
+      (mk_line ~width:3 ~color:Draw.(opaque blue) ()))) 
+in
+let flight_button = W.button ~border_radius:10 "One-way-flight" in
+let f_button_lay = L.resident flight_button in
+  let start_date = W.text_input ~prompt: " " () in
+  let start_layout = L.resident ~w:100 start_date ~background:(Layout.style_bg style)
+in
+  let end_date = W.text_input ~prompt: " " () in
+  let end_layout = L.resident ~w:100 end_date ~background:(Layout.style_bg style)
+in
+let book_button = W.button ~border_radius:10 "Book" in
+let b_button_lay = L.resident book_button in
+  Layout.tower ~name:"Flight Booker" ~align:Draw.Center
+  [f_button_lay; start_layout; end_layout; b_button_lay;]
   |> Bogue.of_layout
   |> Bogue.run
